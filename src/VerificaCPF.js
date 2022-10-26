f3bdd2bdb4dd49b5d8fe02fe278a0fcaa849bc12
luevertonruan <luevertonruanprofissional@gmail.com>
2022-10-25 22:33:25 -0300

    primeiro commit

---
+++ src/VerificaCPF.js
@@ -0,0 +1,53 @@
+var r = require('readline-sync');
+let resultado = 0;
+let soma = 0;
+let limite = 0;
+//let opcaoIniciar = "M";
+/*             ---------------------
+                                    VALIDADOR DE CPF */
+function validaCPF(CPF, multiplicador) {
+  //Gerador de CPF: https://www.4devs.com.br/gerador_de_cpf
+  if (multiplicador == 11) {
+    limite = 1;
+    soma = 0;
+  } else {
+    limite = 2;
+    soma = 0;
+  }
+  for (i = 0; i < CPF.length - limite; i++) {
+    soma += CPF[i] * multiplicador;
+    multiplicador -= 1;
+  }
+  let restoDivisao = soma % 11;
+  if (restoDivisao >= 2) {
+    resultado = 11 - restoDivisao;
+  } else {
+    resultado = 0;
+  }
+  return resultado;
+}
+function executar() {
+  //              -----------------    VALIDADOR DE CPF
+  console.log('Você escolheu o Validador de CPF!\n');
+  let CPF = [9,7,4,9,2,4,1,2,0,7,0];
+  console.log("CPF verificado:"+CPF)
+  /* while (opcaoIniciar == "M") {}
+  let i=0;
+  for (i=0; i<11; i++) {
+      
+    CPF[i] = (r.question(`Informe o ${i + 1}º número do CPF:`));
+  }
+    console.log(CPF);*/
+
+    let digito1 = validaCPF(CPF, 10);
+    let digito2 = validaCPF(CPF, 11);
+    console.log('Primeiro digito verificador:' + digito1 + "\n");
+    console.log('Segundo digito verificador:' + digito2 + "\n");
+    if ((CPF[9] == digito1) & (CPF[10] == digito2)) {
+      console.log('CPF válido!\n');
+    } else {
+      console.log('CPF inválido!\n');
+    }
+  
+}
+executar();
