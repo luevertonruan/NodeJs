e872f338cfe7a80a614275f4c9ea0583d9ff818f
luevertonruan <luevertonruanprofissional@gmail.com>
2022-10-31 00:06:22 -0300

    Correção da Matriz

--- src/Matriz.js
+++ src/Matriz.js
@@ -13,7 +13,7 @@ let c,l;
 let matrizO = [[],[]];
 for (l=0;l < 2;l++) {
   for (c = 0; c < 3; c++) {
-    matrizO[[l],[c]] = Number
+    matrizO[l][c] = Number
       (readlineSync.question(
         `Informe o ${c+1}º número para a ${l+1}ª linha:\n`
       ))  
@@ -23,10 +23,11 @@ for (l=0;l < 2;l++) {
 console.log(`A matriz original informada é: \n ${matrizO}`);
 
 
-let matrizT = [[],[]];
+let matrizT =[];
 for (l = 0; l < 3; l++) {
+  matrizT[l]=[];
   for (c = 0; c < 2; c++) {
-    matrizT[[l],[c]]= matrizO[[c],[l]]
+    matrizT[l][c]= matrizO[c][l]
   }
 }/**/
 console.log(`A matriz transposta correspondente é:\n ${matrizT}`);
--- src/Programa.js
+++ src/Programa.js
@@ -1,25 +1,16 @@
 var readlineSync = require("readline-sync");
+let opcaoIniciar = "M";
+let opcaoErrada = "Você informou uma opção incorreta!\n";
 
 /*                                  -------------------------------  
                                                                                         MENÚ PRINCIPAL!!!*/
 function menuPrincipal() {
-  let listaOpcao =
-    "\n>>> CALCULADORA MULTIFUNCIONAL <<<\n" +
-    "A - Calculadora tradicional\n" +
-    "B - Validador de CPF\n" +
-    "C - Validador de CNPJ\n" +
-    "D - Gerador de Matriz Transposta\n" +
-    "E - Quero sair!\n" +
-    "\n    Digite a opção desejada: \n";
-  return listaOpcao;
+  let opcaoMenuPrincipal = readlineSync.question(
+    "\n>>> CALCULADORA MULTIFUNCIONAL <<<\n A - Calculadora tradicional\n B - Validador de CPF e CNPJ\n D - Gerador de Matriz Transposta\n E - Quero sair!\n \n    Digite a opção desejada: \n");
+  return opcaoMenuPrincipal;
 }
 function executar() {
-  let opcaoMenuPrincipal, opcaoRepete;
-  let repete = "zzz";
-  let opcaoIniciar = "M";
-  let opcaoErrada = "Você informou uma opção incorreta!\n";
-
-  opcaoMenuPrincipal = readlineSync.question(menuPrincipal());
+  menuPrincipal();
 
   /*while (opcaoMenuPrincipal!='E'){
         switch (opcaoMenuPrincipal){}*/
